eccc8419b9f8b4140e7b8627393d26832ec0ff82
mjcepeda <mjindiano@gmail.com>
2018-01-28 21:34:14 -0500

    Initial commit

--- src/edu/rit/dao/iapi/relational/RelationalAlgebra.java
+++ src/edu/rit/dao/iapi/relational/RelationalAlgebra.java
@@ -4,5 +4,9 @@ package edu.rit.dao.iapi.relational;
  * The Class RelationalAlgebra.
  */
 public abstract class RelationalAlgebra {
+	
+	public abstract void open();
+	
+	public abstract void close();
 
 }
