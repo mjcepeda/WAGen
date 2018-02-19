3956d5faa5dbe0e3a2241597ca8d061856ac3cde
mjcepeda <mjindiano@gmail.com>
2018-02-18 23:00:21 -0500

    Centralizing code

--- src/edu/rit/wagen/sqp/iapi/operator/RAOperator.java
+++ src/edu/rit/wagen/sqp/iapi/operator/RAOperator.java
@@ -1,7 +1,9 @@
 package edu.rit.wagen.sqp.iapi.operator;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import edu.rit.wagen.database.impl.DatabaseImpl;
 import edu.rit.wagen.dto.Tuple;
 /**
  * Relational Algebra Operator class
@@ -16,23 +18,33 @@ public abstract class RAOperator {
 	public String _realSchema;
 	//verify is the counter has reached the cardinality
 	public int _counter;
-	
+	//cardinality
 	public int _cardinality;
+	//tuple result list
+	public List<Tuple> _results;
 	
 	//May I change this for a list<Attribute>?
 	public List<String> _preGroupedList;
 	
 	public Boolean _isPreGrouped;
 	
-	public abstract void open();
+	public DatabaseImpl db = new DatabaseImpl();
 	
-	public abstract Tuple getNext();
+	public abstract void open() throws Exception;
+	
+	public abstract Tuple getNext() throws Exception;
 	
 	public abstract void close();
 	
 	public RAOperator (String sbSchema, String realSchema) {
 		this._sbSchema = sbSchema;
 		this._realSchema = realSchema;
+		//init variables
+		_counter = 0;
+		_cardinality = 0;
+		_results = new ArrayList<>();
+		_preGroupedList = new ArrayList<>();
+		_isPreGrouped = null;
 	}
 	
 }
